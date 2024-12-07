1bc100d042e7581bc6ad1e879a4e88c315f79280
nitinpython <nitinsharma007007@gmail.com>
2024-12-07 14:43:34 +0530

    Add the game pause function and create pause menu. Also give functionality to each menu button.

--- tetris.py
+++ tetris.py
@@ -11,6 +11,9 @@ class Tetris:
     def __init__(self):
         pg.init()
 
+        # Constants
+        self.playing = True
+
         # Loading images
         self.IMAGES = self.load_images()
 
@@ -45,6 +48,30 @@ class Tetris:
         exit()
 
 
+    @ staticmethod
+    def generate_hover_effect(*args: pg.Rect):
+        
+        # Checking if the mouse hovers on the buttons
+        if (
+            args[0].collidepoint(pg.mouse.get_pos()) or
+            args[1].collidepoint(pg.mouse.get_pos()) or
+            args[2].collidepoint(pg.mouse.get_pos())
+            ):
+            pg.mouse.set_cursor(pg.SYSTEM_CURSOR_HAND)
+
+        else:
+            pg.mouse.set_cursor()
+
+
+    @staticmethod
+    def button_clicked(button: pg.Rect):
+        # Checking if the mouse is clicked while hovering on the button
+        if button.collidepoint(pg.mouse.get_pos()) and pg.mouse.get_pressed()[0]:
+            return True
+        
+        return False
+    
+    
     def event_handler(self):
         
         for event in pg.event.get():
@@ -52,6 +79,13 @@ class Tetris:
             # Close the game
             if event.type == QUIT:
                 self.close_game_window()
+
+            # Keyboard event
+            elif event.type == KEYDOWN:
+                
+                # Pause the game
+                if event.key == K_SPACE:
+                    self.playing = False
 
     
     def run(self):
@@ -62,10 +96,32 @@ class Tetris:
 
             self.SCREEN.fill(BG_COLOR)                  # Refresh the screen
 
-            self.game_surface.run()
-            self.preview_surface.run()
-            self.score_surface.run()
+            if self.playing:
+                self.game_surface.run()
+                self.preview_surface.run()
+                self.score_surface.run()
 
+            # Game paused
+            else:
+                
+                # Blit the menu buttons
+                resume_button = self.SCREEN.blit(self.IMAGES['resume'], RESUME_BUTTON_COORDINATES)
+                restart_button = self.SCREEN.blit(self.IMAGES['restart'], RESTART_BUTTON_COORDINATES)
+                exit_button = self.SCREEN.blit(self.IMAGES['exit'], EXIT_BUTTON_COORDINATES)
+
+                self.generate_hover_effect(resume_button, restart_button, exit_button)
+
+                # Check which button is clicked and perform the action
+                if self.button_clicked(resume_button):
+                    self.playing = True                     # Resume game
+
+                elif self.button_clicked(restart_button):
+                    self.__init__()                         # Restart game
+
+                elif self.button_clicked(exit_button):
+                    self.close_game_window()                # Close the game
+
+            
             pg.display.update()
 
 
